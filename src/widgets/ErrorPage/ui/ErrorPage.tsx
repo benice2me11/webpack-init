b8aee8aebcf63caae485aa30b86a803162bb3fb9
whatever <benice2me11@gmail.com>
2024-03-29 03:21:38 +0300

    error page loader plus minor fixes

---
+++ src/widgets/ErrorPage/ui/ErrorPage.tsx
@@ -0,0 +1,26 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+import cls from './ErrorPage.module.scss';
+
+interface ErrorPageProps {
+    className?: string;
+
+}
+
+export const ErrorPage = ({ className }: ErrorPageProps) => {
+    const { t } = useTranslation();
+
+    const reloadPage = () => {
+        location.reload();
+    };
+
+    return (
+        <div className={classNames(cls.ErrorPage, {}, [])}>
+            <p>{t('Произошла непредвиденная ошибка')}</p>
+            <Button onClick={reloadPage}>
+                {t('Перезагрузить')}
+            </Button>
+        </div>
+    );
+};
