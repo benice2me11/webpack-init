59284527d4263a3eb10b690eef92cb8862b6edd4
whatever <benice2me11@gmail.com>
2024-03-30 03:29:02 +0300

    storybook setup plus own stories plus minor fixes

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,41 +1,56 @@
-import webpack from "webpack";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import {BuildOptions} from "./types/config";
+import webpack from 'webpack';
+import { buildCssLoader } from './loaders/buildCssLoader';
+import { BuildOptions } from './types/config';
 
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    };
 
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
-
-    const cssLoader = {
-
-        test: /\.s[ac]ss$/i,
-            use: [
-        // Creates `style` nodes from JS strings
-        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-        {
-           loader: 'css-loader',
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
             options: {
-               modules: {
-                   auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                   localIdentName: isDev
-                       ? '[path][name]__[local]--[hash:base64:5]'
-                       : '[hash:base64:8]',
-               },
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        'i18next-extract',
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true,
+                        },
+                    ],
+                ],
             },
         },
-        // Compiles Sass to CSS
-        "sass-loader",
-    ],
+    };
 
-    }
+    const cssLoader = buildCssLoader(isDev);
 
+    // Если не используем тайпскрипт - нужен babel-loader
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-    }
+    };
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    };
 
     return [
+        fileLoader,
+        svgLoader,
+        babelLoader,
         typescriptLoader,
-        cssLoader
-    ]
+        cssLoader,
+    ];
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,13 +1,12 @@
-import HTMLWebpackPlugin from "html-webpack-plugin";
-import path from "path";
-import webpack from "webpack";
-import {BuildOptions} from "./types/config";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import { BuildOptions } from './types/config';
 
-export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
-
+export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
-        new HTMLWebpackPlugin({
+        new HtmlWebpackPlugin({
             template: paths.html,
         }),
         new webpack.ProgressPlugin(),
@@ -15,5 +14,12 @@ export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstan
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
         }),
-    ]
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
+    ];
 }
