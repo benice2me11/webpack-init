1cef41ec31b3d0ed7bda7707c249114b2f4aea93
whatever <benice2me11@gmail.com>
2024-03-30 05:43:07 +0300

    setup loki plus minor screens

--- src/pages/AboutPage/AboutPage.async.tsx
+++
@@ -1,5 +0,0 @@
-import {lazy} from "react";
-
-export const AboutPageAsync = lazy( () => new Promise(resolve => {
-    setTimeout( () => (import('./AboutPage')), )
-}));
--- src/pages/MainPage/MainPage.async.tsx
+++
@@ -1,5 +0,0 @@
-import {lazy} from "react";
-
-export const MainPageAsync = lazy( () => new Promise(resolve => {
-    setTimeout( () => (import('./MainPage')), )
-}));
